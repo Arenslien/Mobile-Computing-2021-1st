9fac9a473ff94661bf0a787411d3572d0ec3fc6d
Arenslien <arenslien@gmail.com>
2021-06-16 14:08:12 +0900

    모바일 컴퓨팅 끝 나중에 미세먼지 어플 API 데이터 받아오는 거 완성할 예정

--- JungSH_60191686_finalProject/app/src/main/java/com/arenslien/jungsh_60191686_finalproject/CityListViewActivity.java
+++ JungSH_60191686_finalProject/app/src/main/java/com/arenslien/jungsh_60191686_finalproject/CityListViewActivity.java
@@ -1,13 +1,22 @@
 package com.arenslien.jungsh_60191686_finalproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class CityListViewActivity extends Activity {
 
@@ -32,11 +41,11 @@ public class CityListViewActivity extends Activity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String key = "Hhm2I7CQQjOyWV0c0GN815XLXfUm68GagTexgRLp9emqiuvPvFDjAfOBiCLCo1pIqMW8LyfBq1h2lPy9fHi9ig%3D%3D";
-                String url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=" + cities[i] + "&pageNo=1&numOfRows=100&returnType=xml&serviceKey=" + key + "&ver=1.0";
+//                String key = "Hhm2I7CQQjOyWV0c0GN815XLXfUm68GagTexgRLp9emqiuvPvFDjAfOBiCLCo1pIqMW8LyfBq1h2lPy9fHi9ig%3D%3D";
+//                String url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=" + cities[i] + "&pageNo=1&numOfRows=100&returnType=xml&serviceKey=" + key + "&ver=1.0";
 
-                Intent intent = new Intent(getApplicationContext(), InformationActivity.class);
-                intent.putExtra("SeletedCityName", cities[i]);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("SelectedCityName", cities[i]);
                 startActivity(intent);
             }
         });
@@ -44,12 +53,68 @@ public class CityListViewActivity extends Activity {
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // 길게 누른 아이템 txt 파일에 저장
+                // 선택한 지역이 이미 즐겨찾기 되어있는지 체크하기
+                if(checkBeforeAdd(cities[i])) {
+                    addFavoriteLocation(cities[i]);
+                    Toast.makeText(getApplicationContext(), "지역 "+ cities[i] +"(이)가 즐겨찾기 되었습니다.", Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(getApplicationContext(), cities[i] + "이(가) 즐겨찾기에 등록되었습니다.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("SelectedCityName", cities[i]);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "이미 즐겨찾기된 지역입니다.", Toast.LENGTH_SHORT).show();
+                }
                 return false;
             }
         });
     }
 
+    protected void addFavoriteLocation(String locationName) {
+        String locationStr = getTxt() + locationName + ",";
+        try {
+            FileOutputStream fos = openFileOutput("location.txt", Context.MODE_PRIVATE);
+            fos.write(locationStr.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected String getTxt() {
+        try {
+            FileInputStream fis = openFileInput("location.txt");
+            byte[] txt = new byte[500];
+            fis.read(txt);
+            String locationStr = new String(txt).trim();
+            fis.close();
+            return locationStr;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    protected String[] txtToArray(String txt) {
+        String[] result = {};
+        result = txt.split(",");
+        return result;
+    }
+
+    protected String arrayToTxt(String[] array) {
+        String txt = "";
+        for(int i=0; i<array.length; i++) {
+            txt = txt + array[i] + ",";
+        }
+        return txt;
+    }
+
+    protected boolean checkBeforeAdd(String item) {
+        String[] array = txtToArray(getTxt());
+        for (int i=0; i<array.length; i++) {
+            if (array[i].equals(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- JungSH_60191686_finalProject/app/src/main/java/com/arenslien/jungsh_60191686_finalproject/MainActivity.java
+++ JungSH_60191686_finalProject/app/src/main/java/com/arenslien/jungsh_60191686_finalproject/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,14 +23,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    // 기본 버튼
-    LocationManager lm;
-    LinearLayout mainLayout;
+    // 이미지, 상태, 색상
+    String[] states = {"좋음", "보통", "나쁨", "매우나쁨"};
+    int[] faceImages = {R.drawable.smile, R.drawable.soso, R.drawable.sad, R.drawable.angry};
+    String[] colors = {"#aaccff", "#aaffaa", "#FFAF71", "#dd5a5a"};
+
+    // 여러 위젯 선언
     TextView locationTxt, statusTxt, dustTxt1, dustTxt2;
+    ImageView faceImg;
+    LinearLayout mainLayout;
+
+    // 현재 위치에 대한 좌표 정보
+    double latitude = 0;
+    double longitude = 0;
+
+    // API 관련 변수
+    String key = "Hhm2I7CQQjOyWV0c0GN815XLXfUm68GagTexgRLp9emqiuvPvFDjAfOBiCLCo1pIqMW8LyfBq1h2lPy9fHi9ig%3D%3D";
+    String returnType = "xml";
+    String numOfRows = "1";
+    String pageNo = "1";
+    String addr = "";
+//    final String[] result = {""};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("미세먼지 정보 조회");
 
-        // 위치 정보 권한 설정
+        // 1. 위치 정보 권한 설정
         try {
             if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
                 ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 100);
@@ -46,28 +71,44 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        // 텍스트뷰 연결
+        // 2. 여러 위젯 연결
         locationTxt = (TextView) findViewById(R.id.locationTxt);
+        faceImg = (ImageView) findViewById(R.id.faceImg);
         statusTxt = (TextView) findViewById(R.id.statusTxt);
         dustTxt1 = (TextView) findViewById(R.id.dustTxt1);
         dustTxt2 = (TextView) findViewById(R.id.dustTxt2);
-
-        // 레이아웃 연결
         mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
 
-        // 이미지, 상태, 색상
-        String[] states = {"좋음", "보통", "나쁨", "매우나쁨"};
-        int[] imgs = {R.drawable.smile, R.drawable.soso, R.drawable.sad, R.drawable.angry};
-        String[] colors = {"#aaccff", "#aaffaa", "#FFAF71", "#dd5a5a"};
+        // 3. 현재 위치 정보 가져오기
+        setLatitude();
+        setLongitude();
+        addr = getLocationName(latitude, longitude);
+
+        // 4. 인텐트 가져오기
+        Intent intent = getIntent();
+        String select = intent.getStringExtra("SelectedCityName");
+        String favorite = intent.getStringExtra("FavoriteCityName");
+
+        // 5. 만약 인텐트 정보가 있다면 인텐트 정보에 대해서 미세먼지 보여주기
+        // 없다면 현재 위치 정보에 대한 미세먼지 정보 보여주기
+        if (select != null) {
+            setDustInformation(select, 77, 36);
+        } else if (favorite != null) {
+            setDustInformation(favorite, 32, 17);
+        } else {
+            setDustInformation(addr, 13, 8);
+        }
 
         // 현재 지역 주변의 측정소 데이터 가져오기
         // getCurrentLocationInfo();
+    }
 
+    private void setDustInformation(String locationName, double dust1, double dust2) {
         // 미세먼지, 초미세먼지, 측정소 주소 데이터 가져오기
-        double dustValue1 = 0;
-        double dustValue2 = 0;
-        String location = "김포시 사우동";
+        String location = locationName;
         int stateI = 0;
+        double dustValue1 = dust1;
+        double dustValue2 = dust2;
 
         if (dustValue2 < 15) {
             stateI = 0;
@@ -82,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
         // 미세먼지 정보 세팅
         locationTxt.setText(location);
         statusTxt.setText(states[stateI]);
+        faceImg.setImageResource(faceImages[stateI]);
         dustTxt1.setText(Double.toString(dustValue1));
         dustTxt2.setText(Double.toString(dustValue2));
         mainLayout.setBackgroundColor(Color.parseColor(colors[stateI]));
@@ -103,19 +145,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 return true;
             case R.id.favoriteLocationMenu:
+                Intent intent2 = new Intent(getApplicationContext(), FavoriteLocationListViewActivity.class);
+                startActivity(intent2);
                 return true;
             case R.id.currentLocationMenu:
-                // GPS Tracker 통해 위도 경도 가져오기
-                LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-                @SuppressLint("MissingPermission")
-                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                double longitude = location.getLongitude();
-                double latitude = location.getLatitude();
-
-                Toast.makeText(getApplicationContext(), getLocationName(latitude, longitude), Toast.LENGTH_LONG).show();
-
-                Intent intent2 = new Intent(getApplicationContext(), InformationActivity.class);
-//                startActivity(intent2);
+                setLatitude();
+                setLongitude();
+                addr = getLocationName(latitude, longitude);
+                // 현재 위치에 대한 정보로 변경
+                setDustInformation(addr, 13, 8);
+                Toast.makeText(getApplicationContext(), "위도: " + latitude + " 경도: " + longitude, Toast.LENGTH_SHORT).show();
                 return true;
         }
         return false;
@@ -123,12 +162,57 @@ public class MainActivity extends AppCompatActivity {
 
     private String getLocationName(double latitude, double longitude) {
         Geocoder geocoder = new Geocoder(MainActivity.this);
+        String result = "";
         List<Address> list = null;
         try {
             list = geocoder.getFromLocation(latitude, longitude, 1);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return list.get(0).toString();
+        if (list.get(0).getLocality() != null) {
+            result = list.get(0).getLocality();
+        } else if (list.get(0).getThoroughfare() != null) {
+            result = list.get(0).getThoroughfare();
+        }
+        return result;
     }
+
+    private void setLatitude() {
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        @SuppressLint("MissingPermission") Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitude = location.getLatitude();
+    }
+
+    private void setLongitude() {
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        @SuppressLint("MissingPermission") Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longitude = location.getLongitude();
+    }
+
+//    private void requestAPI() {
+//        String uri = "http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getMsrstnList?serviceKey=" + key + "&returnType=" + returnType + "&numOfRows=" + numOfRows + "&pageNo=" + pageNo + "&addr=" + addr;
+//
+//        // ...
+//
+//        // Instantiate the RequestQueue.
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//
+//        // Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, uri,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Display the first 500 characters of the response string.
+//                        result[0] = "Response is: "+ response.substring(0,500);
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                result[0] = "That didn't work!";
+//            }
+//        });
+//
+//        // Add the request to the RequestQueue.
+//        queue.add(stringRequest);
+//    }
 }
